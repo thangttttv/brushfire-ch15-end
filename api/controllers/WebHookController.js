3dfe5fbfce378960cc653ff66e9494e52536104e
thangttttv <thang24011983@gmail.com>
2017-12-15 18:07:04 +0700

    no message

--- api/controllers/WebHookController.js
+++ api/controllers/WebHookController.js
@@ -40,6 +40,7 @@ module.exports = {
                         var text = message.message.text;
                         console.log(text); // In tin nhắn người dùng
                         this.sendMessage(senderId, "Tui là bot đây: " + text);
+                        sendQuickReply(senderId);
                     }
                 }
             }
@@ -65,6 +66,70 @@ module.exports = {
                 },
             }
         });
+    },
+
+    /*
+     * Send a message with Quick Reply buttons.
+     *
+     */
+    sendQuickReply :function(recipientId) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: "What's your favorite movie genre?",
+            quick_replies: [
+                {
+                    "content_type":"text",
+                    "title":"Action",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Comedy",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Drama",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+                }
+            ]
+        }
+    };
+
+    callSendAPI(messageData);
+    },
+
+    /*
+     * Call the Send API. The message data goes in the body. If successful, we'll
+     * get the message id in a response
+     *
+     */
+    callSendAPI :function(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: "EAACEdEose0cBAM1BNFMuhGIEyGQbULna4TKWNgmZChhUC664Ez5cJdXOiS09VknXhCWU6C7GLsZAmL8142fP5xyOl0cOZC7VCkfWjSVky34R0c94WrNCNQ3Pyr7T5HXRVuMOrWHuuU0yoWz09PPid5HPrLsxkkW0FjLhXfZBMTjEIIGTZCK8cApZCpvYUwZCSdL7SnVZCBMXYgZDZD" },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            if (messageId) {
+                console.log("Successfully sent message with id %s to recipient %s",
+                    messageId, recipientId);
+            } else {
+                console.log("Successfully called Send API for recipient %s",
+                    recipientId);
+            }
+        } else {
+            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+        }
+    });
     }
 };
 
