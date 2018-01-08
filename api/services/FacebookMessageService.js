b3521d23b9802d90a7f77afd325c7e17bd7385bf
thangttttv <thang24011983@gmail.com>
2018-01-09 00:12:23 +0700

    no message

--- api/services/FacebookMessageService.js
+++ api/services/FacebookMessageService.js
@@ -1,109 +1,12 @@
 /**
- * WebHookController
- *
- * @description :: Server-side logic for managing Webhooks
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ * Created by ThangTT on 09/01/2018.
  */
 
+var token_page = "EAACOHQOP7HgBAIYAIK7U6GUZAzDUySHkzKncGkS6uuCU2cUA0RKN1r9VnpA2ZBWwOojWilWYcffb4z090GumztNelsnCHcjYDKg8LI14gdwZCZCeqS46ZCZAnNHZB19ZAS2tdJmhb4nzaEYPFFrLSm19UXHlzYpy0cDxD5fP9v3Wzgbj4XJYZBgaP";
 const util = require('util')
 var request = require('request');
-var token_page = "EAACOHQOP7HgBAIYAIK7U6GUZAzDUySHkzKncGkS6uuCU2cUA0RKN1r9VnpA2ZBWwOojWilWYcffb4z090GumztNelsnCHcjYDKg8LI14gdwZCZCeqS46ZCZAnNHZB19ZAS2tdJmhb4nzaEYPFFrLSm19UXHlzYpy0cDxD5fP9v3Wzgbj4XJYZBgaP";
 
 module.exports = {
-
-    index: function(req, res) {
-        if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
-            res.send(req.query['hub.challenge']);
-        }
-        res.send('Error, wrong validation token');
-    },
-
-    process: function(req, res) {
-        console.log('121');
-        var entries = req.body.entry;
-        // console.log(util.inspect(entries, false, null))
-
-        request({
-            url: 'https://graph.facebook.com/v2.6/426390950873704?fields=access_token',
-            qs: {
-                access_token: token_page,
-            },
-            method: 'GET',
-            json: {
-            }
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(util.inspect(body));
-                console.log("toenk"+body.access_token);
-                token_page = body.access_token;
-            } else {
-                console.error("Failed calling get token", response.statusCode, response.statusMessage, body.error);
-            }
-        });
-
-
-        for (var i in entries) {
-            var entry = entries[i];
-            var messaging = entry.messaging;
-            console.log(messaging);
-            console.log(messaging[0]);
-            console.log(messaging[0].sender.id);
-
-            //   for (var message of messaging) {
-            for (var j in messaging) {
-                var message = messaging[j];
-                console.log(message);
-                var senderId = message.sender.id;
-                // check message first send
-                    // true
-                    /*
-                    * - get welcome message
-                    * - send welcome
-                    *
-                    * */
-                MessageLog.findOne({where:{sender_id:senderId}}).exec(function(err,message){
-                    if(message==null){
-                        /* */
-                        FacebookMessageService.sendTextMessage(senderId,"xin chao ban");
-                    }
-                });
-                // Check keyword
-                    /*
-                        - keyword in
-                        - keyword A And B
-                        - keyword A Not B
-                        - keyword and
-                     */
-                // End default message
-
-                if (message.message) {
-                    // If user send text
-                    if (message.message.text) {
-                        var text = message.message.text;
-                        console.log(text); // In tin nhắn người dùng
-                        // this.sendMessage(senderId, "Tui là bot đây: " + text);
-                        // this.sendQuickReply(senderId);
-                        // this.sendImageMessage(senderId);
-                        // this.sendAudioMessage(senderId);
-                        // this.sendFileMessage(senderId);
-                        //  this.sendButtonMessage(senderId);
-                        // this.sendVideoMessage(senderId);
-                        console.log("indexoff"+text.indexOf("button"));
-                        if(text.indexOf("button")>=0)
-                        {
-                            this.sendButtonMessage(senderId);
-                        }else{
-                            this.sendGenericMessage(senderId);
-                        }
-
-
-                    }
-                }
-            }
-        }
-
-        res.status(200).send("OK");
-    },
 
     // Gửi thông tin tới REST API để trả lời
     sendMessage: function(senderId, message) {
@@ -383,5 +286,5 @@ module.exports = {
             }
         });
     }
-};
 
+}
